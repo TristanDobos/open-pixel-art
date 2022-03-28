9713b8702db8b60fa729f0b5c70e2f8bdc55ceb8
Tristan Dobos <tristandobos@gmail.com>
2022-03-28 17:59:51 +0200

    A little change

--- scripts/configureMergeDriver.js
+++ scripts/configureMergeDriver.js
@@ -7,6 +7,7 @@ async function run() {
   await exec(
     `git config merge.pixels.name "A custom merge driver for the pixels file"`,
     { shell: true }
+    
   );
   await exec(
     `git config merge.pixels.driver "node scripts/mergePixels.js %O %A %B"`,
